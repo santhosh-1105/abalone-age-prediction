64d2ff38f4827c38f304a43c054c1f4c290d41c6
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-09-29 08:57:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIARUEMFSU6BSCLDKUQ"
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxx"
 }
 
